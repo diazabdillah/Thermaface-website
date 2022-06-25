a0cbf596ff6de94de2b569fc19db7f7d89104a0f
diazabdillah20 <diazabdillah20@gmail.com>
2022-06-25 12:22:25 +0700

    first commit

--- controller/authController.js
+++ controller/authController.js
@@ -2,7 +2,9 @@ const Auth = require("../model/auth")
 
 module.exports = {
   login: function(req, res) {
-      res.render('login')
+      res.render('login',{
+        layout: 'auth/main-auth',
+      })
   },
   register: function(req, res) {
     res.render('register')
--- controller/homeController.js
+++ controller/homeController.js
@@ -1,24 +1,38 @@
 
 module.exports = {
   index: function(req, res) {
-      res.render('index')
+      res.render('index',{
+        layout: 'partials/main-home',
+      })
   },
   about: function(req, res) {
-      res.render('about')
+      res.render('about',{
+        layout: 'partials/main-home',
+      })
   },
   produk: function(req, res) {
-    res.render('product')
+    res.render('product',{
+      layout: 'partials/main-home',
+    })
 },
 testimoni: function(req, res) {
-  res.render('testimoni')
+  res.render('testimoni',{
+    layout: 'partials/main-home',
+  })
 },
 faq: function(req, res) {
-  res.render('faq')
+  res.render('faq',{
+    layout: 'partials/main-home',
+  })
 },
 contact: function(req, res) {
-  res.render('contact')
+  res.render('contact',{
+    layout: 'partials/main-home',
+  })
 },
 login: function(req, res) {
-  res.render('login')
+  res.render('login',{
+    layout: 'partials/main-home',
+  })
 },
 }
